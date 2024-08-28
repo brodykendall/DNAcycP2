14f1fec85f263ae672dd9a8fbc603add179625d0
Brody Kendall <brodykendall15@gmail.com>
2024-08-28 13:30:26 -0500

    added dependency on python scripts

--- R/basilisk.R
+++ R/basilisk.R
@@ -6,4 +6,5 @@ env1 <- BasiliskEnvironment("env1", pkgname="dnacycpv2",
                                        'keras==2.14.0',
                                        'docopt==0.6.2'),
                             pip=c('numpy==1.26.1',
-                                  'bio==1.7.1'))
+                                  'bio==1.7.1'),
+                            paths=c('dnacycpv2_python'))
